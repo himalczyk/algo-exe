b46dd9851787266506fbf588fd4667c52228360d
Michalczyk <dawid.michalczyk@capgemini.com>
2021-11-14 19:58:06 +0100

    New small project ideas created

---
+++ binary_search_algo.py
@@ -0,0 +1,9 @@
+'''
+Create list in range 0-100
+When, User inputs random number, the program check if the number is included in the list by first slicing half of the list.
+If the number is found in the first half, it will eliminate the other one and vice versa.
+The search continues until the program finds the number input of the user or the subarray size becomes 0.
+This would mean that the number is not in the list itself. Range: 0-100. So user cannot provide something else, rule.
+
+
+'''
---
+++ interchange_first_and_last_elements_list.py
@@ -0,0 +1,4 @@
+'''
+Python program to always change the first and last element of a list.
+
+'''
