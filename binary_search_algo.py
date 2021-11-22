782f1250d128ee32839dc766358c20621e86597d
Michalczyk <dawid.michalczyk@capgemini.com>
2021-11-22 16:27:53 +0100

    Started the algo

--- binary_search_algo.py
+++ binary_search_algo.py
@@ -7,3 +7,28 @@ This would mean that the number is not in the list itself. Range: 0-100. So user
 
 
 '''
+
+list_range_100 = list(range(100))
+
+ask_for_number_range_100 = int(input("Please provide a number between 0 and 99\n"))
+found = False
+
+if ask_for_number_range_100 in list_range_100:
+    while found == False:
+        lenght = len(list_range_100)
+        middle_index = lenght//2
+        first_half = list_range_100[:middle_index]
+        second_half = list_range_100[middle_index:]
+        
+        for number in first_half:
+            if ask_for_number_range_100 in first_half:
+                print("first half")
+                found = True
+                break
+        for number in second_half:
+            if ask_for_number_range_100 in second_half:
+                print("second half")
+                found = True
+                break
+else:
+    print("Number not in range from 0-99")
