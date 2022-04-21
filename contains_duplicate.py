ce74ab62b5623aa191a7cbd681a9482dee272642
Michalczyk <dawid.michalczyk@capgemini.com>
2022-04-21 21:12:08 +0200

    contains duplicate algo

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -10,3 +10,14 @@ def containsDuplicate(nums: list) -> bool:
     return False
 
 print(containsDuplicate(nums))
+
+# other solution, faster
+
+def containsDuplicate_two(nums: list) -> bool:
+    hashset = set()
+    
+    for i in nums:
+        if i in hashset:
+            return True
+        hashset.add(i)
+    return False
