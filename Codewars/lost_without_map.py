f81408187b22c72ea0e3031fb9bf48434d4a0c4c
Michalczyk <dawid.michalczyk@capgemini.com>
2022-05-06 17:12:37 +0200

    even more codewars

---
+++ Codewars/lost_without_map.py
@@ -0,0 +1,10 @@
+"""Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]"""
+
+def maps(a):
+    new_arr = [i * 2 for i in a]
+    return new_arr
+        
